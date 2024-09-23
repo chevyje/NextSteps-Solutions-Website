29d5eb03523e4990c30eed22dce61d5fdf9db276
TostiTim <tim.vandebeek@student.hu.nl>
2024-09-23 19:05:40 +0200

    navbar nogsteeds fucked ofzo

---
+++ js/hide-login.js
@@ -0,0 +1,23 @@
+const loginButton = document.getElementById("login-button");
+const profielButton = document.getElementById("profiel-button");
+let logged_in = false;
+
+if(logged_in == true){
+    hideLogin();
+    
+} else {
+    hideProfiel();
+  
+}
+
+function hideLogin(){
+    loginButton.style.display = "none";
+    profielButton.style.display = "inline";
+    console.log("true");
+}
+
+function hideProfiel(){
+    profielButton.style.display = "none";
+    loginButton.style.display = "inline";
+    console.log("false");
+}
