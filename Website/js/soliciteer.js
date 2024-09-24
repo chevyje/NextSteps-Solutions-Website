fbc915e39721e0be549c4f76aaf65480361a124f
DuainCrasto <duaincrasto@hotmail.com>
2024-09-24 13:07:17 +0200

    Solliciteer #7

--- Website/js/soliciteer.js
+++ Website/js/soliciteer.js
@@ -1,18 +1,23 @@
-// Functionality to toggle job description visibility
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButtons = document.querySelectorAll('.toggle-btn');
 
-    toggleButtons.forEach((button, index) => {
+    toggleButtons.forEach((button) => {
         button.addEventListener('click', function() {
             const jobDescription = this.previousElementSibling;
             
             if (jobDescription.style.display === 'block') {
                 jobDescription.style.display = 'none';
-                this.textContent = 'Toon Meer';
+                this.textContent = 'Show More';
             } else {
                 jobDescription.style.display = 'block';
-                this.textContent = 'Toon Minder';
+                this.textContent = 'Show Less';
             }
         });
     });
 });
+
+function applyJob(jobTitle) {
+
+    window.location.href = `apply.html?jobTitle=${encodeURIComponent(jobTitle)}`;
+}
+
